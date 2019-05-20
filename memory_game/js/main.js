4d982268815bbbaa316ac89cf7f5d2b2e744fe38
acupoftee <bdiakaba@uccs.edu>
2019-05-19 18:06:55 -0600

    adding card flip events, score, and reset

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -22,26 +22,71 @@ var cards = [
 	cardImage: "images/king-of-diamonds.png"
 }
 ];
-var cardsInPlay = [];
 
-function checkForMatch() {
-if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
+var cardsInPlay = [];
+var score = 0;
+var win = false;
+var scoreText = document.getElementById('score');
+
+// utility function to add challenge to the game!
+// function shuffleCards() {
+// 	for (var i = 0; i < cards.length; i++) {
+// 		var randomCard = Math.floor((Math.random() * cards.length) + 1);
+// 		var currentCard = cards[i];
+// 	}
+// }
+
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
 	}
 }
 
-function flipCard(cardId) {
-	console.log("User flipped " + cards[cardId].rank);
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
+	console.log("User flipped " + cards[cardId].rank + " of " + cards[cardId].suit);
 	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
 	cardsInPlay.push(cards[cardId].rank);
-
+	this.setAttribute('src', cards[cardId].cardImage);
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}
 }
 
-flipCard(0);
-flipCard(2);
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		// add one point for each match and update the score
+		score++;
+		scoreText.textContent = score;
+		win = true;
+		alert("You found a match! Click the reset to play again!");
+	} else {
+		// resetBard() will reset the score to zero
+		// if done here, the score will be reset before the refresh
+		// for aesthetic reasons, we'll have it reset after the refresh
+		win = false;
+		alert("Sorry, game over. Click reset to play again!");
+	}
+}
+
+function resetBoard() {
+	var images = document.getElementsByTagName('img');
+	for (var i = 0; i < images.length; i++) {
+		images[i].setAttribute('src', "images/back.png");
+	}
+	if (!win) {
+		// reset the score to zero for each loss
+		score = 0;
+		scoreText.textContent = score;
+	}
+	cardsInPlay = [];
+}
+
+var resetButton = document.getElementsByTagName('button')[0];
+resetButton.addEventListener('click', resetBoard);
+
+createBoard();
